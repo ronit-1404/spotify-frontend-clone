cbb229b0b32ce03c5fc9e30620f143a5a0cbaf2a
venom_1404 <ronitdhase@gmail.com>
2024-09-21 10:52:44 +0530

    navbar added

--- spotifyclone/src/main.jsx
+++ spotifyclone/src/main.jsx
@@ -1,14 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//     <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client'; // Import createRoot directly
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+// Use createRoot instead of ReactDOM.createRoot
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-  </StrictMode>,
-)
- 
+  </StrictMode>
+);

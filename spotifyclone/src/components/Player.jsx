7e292c14e2a5b9a2621c16ae36e995e341abf7c3
venom_1404 <ronitdhase@gmail.com>
2024-09-22 15:58:36 +0530

    player error fixed

--- spotifyclone/src/components/Player.jsx
+++ spotifyclone/src/components/Player.jsx
@@ -13,8 +13,8 @@ const Player = () => {
         <div className='flex flex-col items-center gap-1 m-auto'>
           <div className='flex gap-4'>
             <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt="" />
-            <img className='w-4 cursor-pointer' src={assets.prev_icon_icon} alt="" />
-            <img className='w-4 cursor-pointer' src={assets.play_icon_icon} alt="" />
+            <img className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
+            <img className='w-4 cursor-pointer' src={assets.play_icon} alt="" />
             <img className='w-4 cursor-pointer' src={assets.next_icon} alt="" />
             <img className='w-4 cursor-pointer' src={assets.loop_icon_icon} alt="" />
           </div>
